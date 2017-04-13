a2d9470564beca6a7917bd2c6c37ac5bb3481b00
Mike McNeil <michael.r.mcneil@gmail.com>
2017-04-13 13:50:45 -0500

    initial commit to reproduce issue

---
+++ api/models/Token.js
@@ -0,0 +1,25 @@
+/**
+ * Token.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  primaryKey: '_id',
+
+  attributes: {
+
+    _id: {
+      type: 'string',
+      columnName: '_id',
+    },
+
+    objectId: {
+      type: 'string',
+    },
+
+  },
+
+};
